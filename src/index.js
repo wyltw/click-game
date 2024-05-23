900eda4e95954ae26723ff7c476c441b7928ca39
wyltw <138242311+wyltw@users.noreply.github.com>
2024-05-23 11:11:23 +0800

    game screem

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 const titleScreenEl = document.querySelector(".title-screen");
-const tutorialEl = document.querySelector(".main-menu");
-const clickToStart = () => {
-  titleScreenEl.classList.add("hidden");
-  tutorialEl.classList.remove("hidden");
+const mainMenuEl = document.querySelector(".main-menu");
+const mainMenuStartBtnEl = document.querySelector(".main-menu__btn--start");
+const gameScreenEl = document.querySelector(".game-screen");
+const transitionScreen = (hideEl, showEl) => {
+  hideEl.classList.add("hidden");
+  showEl.classList.remove("hidden");
 };
-titleScreenEl.addEventListener("click", clickToStart);
+titleScreenEl.addEventListener("click", () => {
+  transitionScreen(titleScreenEl, mainMenuEl);
+});
+
+mainMenuStartBtnEl.addEventListener("click", () => {
+  transitionScreen(mainMenuEl, gameScreenEl);
+});
